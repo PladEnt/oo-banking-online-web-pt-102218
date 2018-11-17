9844b4826cc2c18f8cbdb644fed8791d4adf548c
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-17 16:17:56 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,11 @@
 class BankAccount
-attr_accessor :name
+  attr_accessor :name
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+  end
+
+  def balance
+  
+  end
 end
