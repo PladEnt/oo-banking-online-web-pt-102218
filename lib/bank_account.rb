4897b48004682893502cf573b1dbee9824fd2cbd
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-17 16:26:26 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,14 @@
 class BankAccount
-  attr_accessor :balance, :status, :amount
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
-    @amount = 0
   end
 
   def deposit(num)
-    @amount + num
+    @balance + num
   end
 end
